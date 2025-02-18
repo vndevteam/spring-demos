822cedb934a9f41e8319efaa977066670cd1079a
Lam Ngoc Khuong <lam.ngoc.khuong@sun-asterisk.com>
2025-02-18 17:08:01 +0700

    feat: add git hooks

--- application-runner/src/test/kotlin/io/github/vndevteam/applicationrunner/ApplicationRunnerApplicationTests.kt
+++ application-runner/src/test/kotlin/io/github/vndevteam/applicationrunner/ApplicationRunnerApplicationTests.kt
@@ -1,13 +1,4 @@
 package io.github.vndevteam.applicationrunner
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
 class ApplicationRunnerApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
 }
